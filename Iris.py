e6a276249f07018d4a210f713bdfdb8b8397445e
momii69 <131962405+momii69@users.noreply.github.com>
2023-04-27 16:17:45 +0500

    Add files via upload

---
+++ Iris.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# In[3]:
+
+
+iris=pd.read_csv("C:\\Users\\My Pc\\Downloads\\iris.csv")
+
+
+# In[4]:
+
+
+iris
+
+
+# In[5]:
+
+
+iris.plot()
+plt.grid()
+
+
+# In[ ]:
+
+
+
+
